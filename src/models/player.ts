a4fca39e2926799c038a0251c04dc785ae5813a9
Patrick Sterz <psterz98@gmail.com>
2022-09-30 13:36:41 +0200

    added Player-class

---
+++ src/models/player.ts
@@ -0,0 +1,14 @@
+export class Player {
+    name!: any;
+    player_image!: string;
+    playerIndex!: number;
+
+
+    constructor() {
+        this.name = name;
+        this.player_image = this.player_image;
+        this.playerIndex = 0;
+        this.playerIndex++;
+    }
+
+}
